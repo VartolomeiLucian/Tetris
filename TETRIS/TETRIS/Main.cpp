bb3ae2b32f820eccacf0af4db60fe0a73ace2367
VartolomeiLucian <lucianv_16@yahoo.com>
2017-01-04 19:46:41 +0200

    Shape on Board Game

--- TETRIS/TETRIS/Main.cpp
+++ TETRIS/TETRIS/Main.cpp
@@ -1,21 +1,57 @@
 #include <SFML/Graphics.hpp>
-#include "Menu.h"
 #include <iostream>
+#include "Menu.h"
+#include "Shapes.h"
 using namespace sf;
 using namespace std;
 
 
- 
+void fillShapesMatrix(bool matrix[4][4], int shape, int rotatie)
+{
+	int i, j;
+	for (int i = 0; i < 4; i++)
+		for (int j = 0; j < 4; j++)
+			matrix[i][j] = shapesM[shape][rotatie][i][j];
+}
+
+void generateShapes(bool matrix[4][4], int &color, int &rotatie, int &shape)
+{
+	color = rand() % 6;
+	rotatie = rand() % 4;
+	shape = rand() % 7;
+	fillShapesMatrix(matrix, shape, rotatie);
+}
+
+
+
+
+
 int main()
 {
 
-	RenderWindow window(VideoMode(750,900), "TETRIS");
+	bool matrix[4][4];
+	int boardGame[16][10] = { 0 };
+	int rotatie, shape, color;
+
+	Vector2f shapePos, windowSize;
+
+
+
+	RenderWindow window(VideoMode(450, 700), "TETRIS");
+	windowSize.x = (float)window.getSize().x;
+	windowSize.y = (float)window.getSize().y;
+	
+	shapePos.x = windowSize.x / BOARD_GAME_SIZE/ 3;
+	shapePos.y = 0;
+
+
 	Menu menu(window.getSize().x, window.getSize().y);
 
-	Texture tbackground;
+	Texture tbackground,tshape;
 	tbackground.loadFromFile("back.jpg");
+
 	Sprite background(tbackground);
-	
+
 	
 
 	while (window.isOpen())
@@ -23,7 +59,7 @@ int main()
 		Event windowEvent;
 		while (window.pollEvent(windowEvent))
 		{
-			
+
 			switch (windowEvent.type)
 			{
 			case Event::KeyReleased:
@@ -42,15 +78,24 @@ int main()
 
 			case Event::Closed:
 				window.close();
-
-
 				break;
 			}
 
 			window.clear();
-			window.draw(background);
-			menu.drawMenu(window); 
+			
+			
+
+
+			generateShapes(matrix, color, rotatie, shape);
+			for (int i = 0; i < 4; i++)
+				for (int j = 0; j < 4; j++)
+					if (matrix[i][j] == 1)
+						drawShapes(window, (j + shapePos.x)*BOARD_GAME_SIZE, (i + shapePos.y)*BOARD_GAME_SIZE, shapeColors[color]);
+
+
+		
 			//window.draw(background);
+			//menu.drawMenu(window);
 			window.display();
 
 		}
@@ -58,6 +103,3 @@ int main()
 
 
 }
-
-
-
